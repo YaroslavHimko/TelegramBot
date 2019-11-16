e8a76d5f77ac478922bfbac8294f58938367bd4d
Yaroslav <yaroslav.himko@gmail.com>
2019-11-17 00:17:25 +0300

    Refactoring pt.4, moved to a separated folder

--- venv/bot.py
+++ venv/bot.py
@@ -1,25 +1,26 @@
 import telebot
-import level
-import user
-import greetings
+from src import level
+from src import user
+from resources import greetings
 import os
-import json_worker
-import passed_levels
-import photos
+from src import json_worker
+from src import passed_levels
+from resources import photos
 
 token = os.environ.get('TelegramToken')
 bot = telebot.TeleBot(token)
 
+#bot = telebot.TeleBot('916863111:AAGWw4dubDgRIszatOgV3MlQFJf-I88FTs4')
+
+
 
 def hello_message(message):
-    print('calling hello message')
     bot.send_message(message.chat.id, greetings.one)
     bot.send_message(message.chat.id, greetings.two)
 
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
-    print('calling command start')
     hello_message(message)
     current_user = user.create_user(message.from_user.id, message.chat.first_name)
     process_level(message, current_user)
@@ -33,13 +34,11 @@ def catching_excessive_text(message):
 
 
 def process_incorrect_content(message, curr_user):
-    print('calling process incorrect text')
     bot.send_message(message.from_user.id, greetings.incorrect_content)
     bot.register_next_step_handler_by_chat_id(message.chat.id, check_content, curr_user)
 
 
 def process_level(message, curr_user):
-    print('calling process level')
     if curr_user.is_finished():
         bot.send_photo(message.chat.id, photo=open('resources/photos/{}'.format(photos.grats), 'rb'))
         bot.send_message(message.chat.id, greetings.grats)
@@ -52,14 +51,12 @@ def process_level(message, curr_user):
 
 
 def user_completed_level(message, curr_user):
-    print('calling user completed level')
     bot.send_message(message.chat.id, greetings.correct_answer + curr_user.show_bats())
     curr_user.get_new_level()
     process_level(message, curr_user)
 
 
 def check_answer(message, curr_user):
-    print('calling check asnwer')
     if curr_user is None:
         curr_user = user.get_user(message.from_user.id, message.chat.first_name)
     user_message = message.text.lower()
@@ -74,17 +71,22 @@ def check_answer(message, curr_user):
 
 
 def check_content(message, curr_user):
-    print('calling check content')
+    print("Entered to check content")
     if message.content_type == 'text':
+        print("Reached text content condition")
         if message.text == '/start' or message.text == '/reset':
+            print("Reached start or reset condition")
             command_start(message)
         else:
-            print(curr_user)
+            print("User content is not a start or reset")
             if not curr_user.is_finished():
+                print("User didn't finish the quest")
                 check_answer(message, curr_user)
             else:
+                print("User completed level")
                 user_completed_level(message, curr_user)
     else:
+        print("User content is not a text")
         process_incorrect_content(message, curr_user)
 
 
---
+++ venv/src/level.py
@@ -0,0 +1,25 @@
+from resources import questions
+from resources import answers
+from resources import photos
+import random
+from resources import greetings
+
+
+class Level:
+    def __init__(self, question, answer, photo):
+        self.question = question
+        self.answer = answer
+        self.photo = photo
+
+
+def get_random_wrong_answer():
+    wrong_answer = random.choice(answers.wrong_answers)
+    return wrong_answer
+
+
+def get_levels_length():
+    return len(levels_dict) - 1
+
+
+def get_level_object(level):
+    return levels_dict.get(level)
